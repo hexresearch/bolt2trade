7cd4fb8574b069d6d887172ce0b4aec85c8d7b99
Aminion <aminion@protonmail.com>
2022-02-17 21:43:33 +0200

    Add frontend base #4

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 #[macro_use] extern crate rocket;
+use rocket::fs::FileServer;
+use rocket_dyn_templates::{Template, context};
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> Template {
+    Template::render("index", context!{
+        parent : "base"
+    })
 }
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+      .mount("/", FileServer::from("static"))
+      .mount("/", routes![index])
+      .attach(Template::fairing())
 }
